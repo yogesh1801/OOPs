6c7d94b6b1de660c8520bfa99348c1184649518f
yogesh1801 <114429499+yogesh1801@users.noreply.github.com>
2023-09-14 16:54:04 +0530

    Lab 3

---
+++ Lab 2/src/DictionaryApp/DictClass.java
@@ -0,0 +1,91 @@
+package DictionaryApp;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Collections;
+public class DictClass {
+    int size = 100; // available size the dictionary
+    int length = 0; // Count of records in the dictionary
+    RecordClass[] Record = new RecordClass[100];
+    int[] keys = new int[100];
+
+    private void sortArray () {
+        Arrays.sort(keys); //  using sorting a reversing
+        reverse(keys);
+        System.out.println(Arrays.toString(keys));
+    }
+
+    private void reverse (int [] array){
+        int n = array.length;
+        for (int i = 0; i < n / 2; i++) {
+
+            int temp = array[i];
+            array[i] = array[n - i - 1];
+            array[n - i - 1] = temp;
+    }
+    }
+    void getKey ( int key) {
+        sortArray();
+        int keyFound = Arrays.binarySearch(keys , key);
+        System.out.println(keyFound);
+        if (keyFound < 0) {
+            System.out.println("No such key found");
+        }else {
+            for (int i=0 ; i<length ; i++) {
+                if(Record[i].key == key){
+                    System.out.println("Key found at index " + keyFound + " with value " + Record[i].value);
+                    break;
+                }
+            }
+        }
+    }
+
+    void putRecord (double value) {
+        sortArray();
+        int key = (int)value;
+        int keyFound = Arrays.binarySearch(keys , key);
+        if (keyFound < 0) {
+            keys[length] = key;
+            Record[length] = new RecordClass(value);
+            length = length + 1;
+            size = size -1;
+            System.out.println("Key created successfully");
+        }else{
+            for (int i=0 ; i < length ; i++){
+                if(Record[i].key == key){
+                    Record[i].value = value;
+                    break;
+                }
+            }
+            System.out.println("Key updated successfully");
+        }
+    }
+
+    void removeRecord (int key){
+        sortArray();
+        int keyFound = Arrays.binarySearch(keys , key);
+        if (keyFound < 0) {
+            System.out.println("No such Key exists");
+        }else {
+            keys[keyFound] =0;
+            for (int i=0 ; i< length ; i++){
+                if(Record[i].key == key){
+                    Record[i].key = 0;
+                    Record[i].value =0;
+                    System.out.println("Record Removed Successfully");
+                    break;
+                }
+            }
+            //length = length +1;
+            //size = size -1;
+        }
+        sortArray();
+        //Arrays.sort(Record); // Trying to Tackle problem of empty spaces left in the objects
+    }
+
+    void records () {
+        for(int i=0 ; i < length ; i++) {
+            System.out.println("Record no " + (i+1) + " key:" + Record[i].key + " value:" + Record[i].value );
+        }
+    }
+}
